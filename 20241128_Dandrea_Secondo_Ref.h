e4c3dc46809b635787ee09c63d27748b52fd6016
studentelig <studentelig@LIGST10>
2024-12-06 11:09:52 +0100

    Fix

--- 20241128_Dandrea_Secondo_Ref.h
+++ 20241128_Dandrea_Secondo_Ref.h
@@ -13,6 +13,34 @@ class Node{
         left=nullptr;
         right=nullptr;
     }
+
+    Node* InsertR(int k){
+        if(k>value){
+            if(right==NULL){
+                right=new Node(k);
+            }else{
+                right->InsertR(k);
+            }
+
+        }else{
+            if(left==NULL){
+                left=new Node(k);
+            }else{
+                left->InsertR(k);
+            }
+        }
+        return this;
+    }
+
+    void InOrder(){
+        if(left!=NULL){
+            left->InOrder();
+        }
+        cout<<value<<endl;
+        if(right!=NULL){
+            right->InOrder();
+        }
+    }
     
     bool searchI(int k){
         Node* current=this;
